8e548621524c97b5db5ba7c86322bcf819e3dc59
LuisAntonioViteri <55764726+LuisAntonioViteri@users.noreply.github.com>
2019-11-19 15:35:05 -0500

    Update arreglos.c

--- segundo semestre/arreglos.c
+++ segundo semestre/arreglos.c
@@ -1,17 +1,17 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 # define tam 10
 
 void inicializarArreglo(int[]);
 void datosVector(int);
+void busquedaInfo(int);
 
 int main()
 {
    int arreglo[tam]={2,4,5,-1},cant,o;//Inicializa el arreglo
    
    do{
-   printf("*****Menu Principal*****\n1.Inicializacion Arreglo\n2.Datos Vector\n3.Salir\n");
+   printf("*****Menu Principal*****\n1.Inicializacion Arreglo\n2.Datos Vector\n3.Busqueda elementos\n4.Salir\n");
     scanf("%i",&o);
     
     switch(o){
@@ -30,8 +30,15 @@ int main()
             system("clear");
         break;
         case 3:
-            exit(0);
+            printf("Ingrese la cantidad de elementos del vector:\n");
+           scanf("%i",&cant);
+            busquedaInfo(cant);
+            getchar();
+            getchar();
+            system("clear");
         break;
+        case 4:
+            exit(0);
         default:
             printf("Escoga una opcion valida\n");
             getchar();
@@ -39,7 +46,7 @@ int main()
             system("clear");
     }
         
-   }while(o!=3);
+   }while(o!=4);
 
    
 
@@ -67,4 +74,26 @@ void datosVector(int cant){
         printf("Elemento[%i]=%i\n",c,arreglo[c]);
         c++;
     }
+}
+
+void busquedaInfo(int cant){
+    int c=0,arreglo[tam],num,rep=0;
+    while(c<cant){
+        printf("Ingrese el elemento[%i]:",c);
+        scanf("%i",&arreglo[c]);
+        c++;
+    }
+    
+    printf("Ingrese el numero a buscar:\n");
+    scanf("%i",&num);
+    c=0;
+    while(c<cant){
+        if(num==arreglo[c]){
+            rep++;
+        }
+        c++;
+    }
+    printf("El numero %i se repite %i veces\n",num,rep);
+    
+    
 }
